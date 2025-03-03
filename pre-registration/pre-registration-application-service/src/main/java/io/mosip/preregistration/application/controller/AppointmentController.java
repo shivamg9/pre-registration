18fd41505bcb9000ad58b452851f3fb53ecb6d28
Shivam Rajesh Gupta <56411166+shivamg9@users.noreply.github.com>
2025-03-03 23:43:47 +0530

    Update AppointmentController.java
    Signed-off-by: Shivam Rajesh Gupta <56411166+shivamg9@users.noreply.github.com>

--- pre-registration/pre-registration-application-service/src/main/java/io/mosip/preregistration/application/controller/AppointmentController.java
+++ pre-registration/pre-registration-application-service/src/main/java/io/mosip/preregistration/application/controller/AppointmentController.java
@@ -37,10 +37,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import springfox.documentation.annotations.ApiIgnore;
-import io.mosip.preregistration.application.service.NotificationService;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @Tag(name = "appointment-controller", description = "Appointment Controller")
@@ -159,17 +155,5 @@ public class AppointmentController {
 			@RequestHeader(value = "User-Agent") String userAgent) {
 		return ResponseEntity.status(HttpStatus.OK).body(appointmentService.makeMultiAppointment(bookingRequest, userAgent));
 	}
-@Autowired
-private NotificationService yourScheduledService;  // Inject Scheduled Task Service
-
-@GetMapping("/applications/appointment/run-reminder-task")
-public ResponseEntity<String> runScheduledTaskManually() {
-    String currentTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-    System.out.println("📌 Task Triggered at: " + currentTime);
-    yourScheduledService.sendAppointmentReminders();
-    return ResponseEntity.ok("✅ Reminder Task Executed Successfully!");
-}
-
-
 
 }
